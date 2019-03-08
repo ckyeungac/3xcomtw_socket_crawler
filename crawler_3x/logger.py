11998712db32d1e2dbee2dfeeb71de5f770e9514
Chun-Kit Yeung <ckyeungac@gmail.com>
2019-03-09 02:06:56 +0800

    bug fix in logging file name

--- crawler_3x/logger.py
+++ crawler_3x/logger.py
@@ -6,7 +6,7 @@ logger = logging.getLogger('3xdotcom_websocket_crawler')
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s -  %(message)s')
 # create file handler which logs even debug messages
-fh = logging.FileHandler('./logs/crawler_3x-{}.log'.format(datetime.datetime.now().date))
+fh = logging.FileHandler('./logs/crawler_3x-{}.log'.format(datetime.datetime.now().date()))
 fh.setLevel(logging.INFO)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
