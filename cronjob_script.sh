1fdda7777aa0156e9662fbc58cbffc8f78c9ad04
Chun-Kit Yeung <ckyeungac@gmail.com>
2019-03-12 16:04:10 +0000

    update the script for running the cronjob

--- cronjob_script.sh
+++ cronjob_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 source /home/chunkit/.bashrc
 source activate trading
-screen -XS trading quit \
-  && screen -dmS trading \
-  && screen -S trading -X stuff \
-      "source activate trading \
-       && cd /home/chunkit/projects/3xcomtw_socket_crawler \
-       && bash /home/chunkit/projects/3xcomtw_socket_crawler/start_crawling_script.sh \n"
+screen -XS trading quit
+sleep 1
+screen -dmS trading \
+&& screen -S trading -X stuff \
+     "source activate trading \
+      && cd /home/chunkit/projects/3xcomtw_socket_crawler \
+      && bash /home/chunkit/projects/3xcomtw_socket_crawler/start_crawling_script.sh \n"
 
